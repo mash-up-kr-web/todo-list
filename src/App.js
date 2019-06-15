284fd63072eb19a0813b5e7e9b7ffc961abf23a7
chayeoi <chayeoikeem@gmail.com>
2019-06-15 14:16:40 +0900

    add todo feature

--- src/App.js
+++ src/App.js
@@ -15,19 +15,39 @@ const S = {
 };
 
 const App = () => {
+  const [value, setValue] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const handleChange = event => setValue(event.target.value);
+  const handleClick = () => {
+    setTodos(prevTodos => [
+      ...prevTodos,
+      { id: prevTodos.length, content: value },
+    ]);
+    setValue('');
+  };
+
   return (
     <S.Wrapper>
       <S.Title>Todo List</S.Title>
       <S.Label htmlFor="todo">할 일: </S.Label>
-      <S.Input type="text" id="todo" />
-      <S.Button type="button">추가</S.Button>
+      <S.Input type="text" id="todo" value={value} onChange={handleChange} />
+      <S.Button type="button" onClick={handleClick}>추가</S.Button>
       <S.TodoList>
+        {todos.map(todo => (
+          <S.TodoItem key={todo.id}>
+            {todo.content}
+          </S.TodoItem>
+        ))}
       </S.TodoList>
-      <S.Paragraph>
-        오늘 할 일을 3개 등록하셨네요!
-      </S.Paragraph>
+      {todos.length === 3 && (
+        <S.Paragraph>
+          오늘 할 일을 3개 등록하셨네요!
+        </S.Paragraph>
+      )}
     </S.Wrapper>
   )
 };
 
 export default App;
+
